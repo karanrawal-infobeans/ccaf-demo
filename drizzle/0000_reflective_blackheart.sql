745e54d400caa8a8ec225a47f254296f09ae0ecb
karan <karan.rawal@infobeans.com>
2026-09-01 23:33:32 +0530

    database created

--- drizzle/0000_reflective_blackheart.sql
+++ drizzle/0000_reflective_blackheart.sql
@@ -1,6 +1,6 @@
 CREATE TYPE "public"."role" AS ENUM('CUSTOMER', 'ADMIN', 'CUSTOMER_SUPPORT');--> statement-breakpoint
 CREATE TABLE "users" (
-	"id" varchar(24) PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY NOT NULL,
 	"email" varchar(255) NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"password" text NOT NULL,
